b3d0abc6c36baf2292cd22905102b95dda53b71e
MosesLawn <suepc@home.local>
2020-04-19 19:03:18 -0400

    added img dir and off.py main source file

---
+++ off.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""OFFICE TOOLKIT v1.0
+
+CODED ON:
+	04.19.2020
+CODED BY:
+	MosesLawn
+CODE ENV:
+	python 3.7 win10x64 Geany
+GITHUB:
+	https://github.com/vji1976/off.git
+DEPENDENCIES:
+	pip install Pillow
+	pip install tkcalendar
+	pip install python-docx
+	pip install selenium
+	pip install beautifulsoup4
+"""
+
